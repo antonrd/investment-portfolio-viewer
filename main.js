b2e9b1005045aaa8b0f8f84a9da8a2932c74f789
Anton Dimitrov <dimitrov.anton@gmail.com>
2015-05-24 20:31:07 +0300

    Adds the initial version of the visualization tool for a portfolio consisting of stocks.

---
+++ main.js
@@ -0,0 +1,40 @@
+var Main = {
+  run: function(do_filter) {
+    ParseStocks.parse('stocks.csv', function(stocks) {
+      ParseTrades.parse('trades.csv', function(trades) {
+        var filtered_stocks = stocks;
+
+        if (do_filter) {
+          filtered_stocks = FilterStocks.filter(stocks);
+        }
+
+        ShowFilters.show(stocks, filtered_stocks.map(function(value) {
+          return value.symbol;
+        }));
+
+        ComputeSummaries.init(filtered_stocks, trades);
+        var stock_summaries = ComputeSummaries.getStockSummaries();
+
+        ShowStockSummaries.show(stock_summaries);
+
+        ComputeBreakdowns.init(stock_summaries, filtered_stocks);
+        var stock_breakdowns = ComputeBreakdowns.getStockBreakdowns();
+
+        ShowStockBreakdowns.show(stock_breakdowns);
+      });
+    });
+  }
+};
+
+(function() {
+
+  if(typeof(String.prototype.trim) === "undefined")
+  {
+      String.prototype.trim = function()
+      {
+          return String(this).replace(/^\s+|\s+$/g, '');
+      };
+  }
+
+  Main.run(false);
+})();
