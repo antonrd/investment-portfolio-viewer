e289ebc6ae6a6bc2b26b7a726decdfa8aa1fb622
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2017-03-02 11:36:04 +0200

    Moving the last callback from main.js to it's own function in order to escape the callback hell. Some other small changes and fixes.

--- main.js
+++ main.js
@@ -1,43 +1,48 @@
 var Main = {
   run: function(do_filter) {
+    let self = this;
     let parseStocks = new ParseStocks(Constants.STOCKS_FILE, function(stocks) {
       let parseTrades = new ParseTrades(Constants.TRADES_FILE, function(trades) {
-        let parseDividends = new ParseDividends(Constants.DIVIDENDS_FILE, function(dividends){
-          var filtered_stocks = stocks;
-
-          if (do_filter) {
-            let filterStocks = new FilterStocks(stocks);
-            filtered_stocks = filterStocks.filter(stocks);
-          }
-
-          let filters = new ShowFilters(stocks, filtered_stocks.map(function(value) {
-            return value.symbol;
-          }));
-          let showFilters = filters.show();
-
-          let computeSummaries = new ComputeSummaries(filtered_stocks, trades, dividends);
-          let stockSummaries = computeSummaries.getStockSummaries();
-
-          let showStockSummaries = new ShowStockSummaries(stockSummaries);
-          let showSummaries = showStockSummaries.show();
-
-          let computeBreakdowns = new ComputeBreakdowns(stockSummaries, filtered_stocks);
-          let stockBreakdowns = computeBreakdowns.getStockBreakdowns();
-
-          let showStockBreakdowns = new ShowStockBreakdowns(stockBreakdowns);
-          let showBreakdowns = showStockBreakdowns.show();
-
-          let computeNextTrades = new ComputeNextTrades(stockBreakdowns);
-          let nextTrades = computeNextTrades.computeTrades();
-
-          let showNextTrades = new ShowNextTrades(nextTrades);
-          let showTrades = showNextTrades.show();
+        let parseDividends = new ParseDividends(Constants.DIVIDENDS_FILE, function(dividends) {
+          self.computeAndShow(dividends, stocks, trades, do_filter);
         });
         parseDividends.parse();
       });
       parseTrades.parse();
     });
     parseStocks.parse();
+  },
+
+  computeAndShow: function(dividends, stocks, trades, do_filter){
+    var filtered_stocks = stocks;
+
+    if (do_filter) {
+      let filterStocks = new FilterStocks(stocks);
+      filtered_stocks = filterStocks.filter(stocks);
+    }
+
+    let filters = new ShowFilters(stocks, filtered_stocks.map(function(value) {
+      return value.symbol;
+    }));
+    let showFilters = filters.show();
+
+    let computeSummaries = new ComputeSummaries(filtered_stocks, trades, dividends);
+    let stockSummaries = computeSummaries.getStockSummaries();
+
+    let showStockSummaries = new ShowStockSummaries(stockSummaries);
+    let showSummaries = showStockSummaries.show();
+
+    let computeBreakdowns = new ComputeBreakdowns(stockSummaries, filtered_stocks);
+    let stockBreakdowns = computeBreakdowns.getStockBreakdowns();
+
+    let showStockBreakdowns = new ShowStockBreakdowns(stockBreakdowns);
+    let showBreakdowns = showStockBreakdowns.show();
+
+    let computeNextTrades = new ComputeNextTrades(stockBreakdowns);
+    let nextTrades = computeNextTrades.computeTrades();
+
+    let showNextTrades = new ShowNextTrades(nextTrades);
+    let showTrades = showNextTrades.show();
   }
 };
 
